e19e3aadf556358c11917560bce4488d84bb37b8
s10116 <s10116@pjwstk.edu.pl>
2013-02-01 18:53:08 +0100

    05

--- src/main/java/registerOffice/Main.java
+++ src/main/java/registerOffice/Main.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import registerOffice.businessObjects.clients.*;
-import registerOffice.businessObjects.films.*; 
+//import registerOffice.businessObjects.films.*; //dubel
 import registerOffice.businessObjects.films.BluRay;
 import registerOffice.businessObjects.films.DVD;
 import registerOffice.businessObjects.films.Film;
