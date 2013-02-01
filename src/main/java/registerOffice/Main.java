44229ae20890771023f73f8eae12c7ae92bb5138
s10116 <s10116@pjwstk.edu.pl>
2013-02-01 17:24:49 +0100

    04

--- src/main/java/registerOffice/Main.java
+++ src/main/java/registerOffice/Main.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 import registerOffice.businessObjects.clients.*;
+import registerOffice.businessObjects.films.*; 
 import registerOffice.businessObjects.films.BluRay;
 import registerOffice.businessObjects.films.DVD;
 import registerOffice.businessObjects.films.Film;
@@ -20,7 +20,7 @@ import registerOffice.management.conditions.Condition;
 import registerOffice.management.conditions.GetByAddressCondition;
 import registerOffice.management.conditions.GetByNameCondition;
 */
-import registerOffice.businessObjects.films.*;
+
 
 public class Main {
 
@@ -36,29 +36,68 @@ public class Main {
 				
 		
 		
-		//DVD
-		Klient jan_kow = new Klient("Jan Kowalski", "80121212123456", "Mila 11");
-		Film td = new DVD("Ted","2013TD");
-		jan_kow.getFilms().add(td);
-		td.setOwner(jan_kow);
-		hib.save(jan_kow);
+		//tworzenie klientow       // ('imie nazwisko','pesel', 'ulica')
+		Klient jan_kow = new Klient("Jan Kowalski", "79010123456", "Mila 11");
+		Klient mar_now = new Klient("Marek Nowak", "80020225256", "Szara 22");
+		Klient ste_bob = new Klient("Stefan Bobowski", "81030314266", "Ciepla 33");
+		Klient ann_kwi = new Klient("Anna Kwiat", "82040431426", "Dobra 44");
+		Klient paw_fle = new Klient("Pawel Flemowski", "83050531789", "Zla 55");
+		Klient ola_mal = new Klient("Ola Mala", "84060631790", "Zupa 66");
+		Klient mir_wie = new Klient("Mirek Wielki", "85070731790", "Danie 77");
 		
-		//BluRay
-		Klient earl = new Klient("Earl", "81111111111346", "Dobra 22");
-		Film sw = new BluRay("Star","2012SW", "HD"); // yes to HD
-		earl.getFilms().add(sw);
-		sw.setOwner(earl);
-		hib.save(earl); 
+		
+		
+		//dodawanie DVD   // ('nazwa filmu','sygnatura filmu')
+		Film TD = new DVD("Ted","2012TD"); 
+		Film AePr = new DVD("Absolute Power","1997AePr");
+		Film Un = new DVD("Unforgiven","1992Un");
+		
+		
+		//dodawanie BluRay  // ('nazwa filmu','sygnatura filmu', 'informacja o HD')
+		Film SrWs = new BluRay("Star Wars","2011SrWs", "HD"); // say yes to HD
+		Film GnTo = new BluRay("Gran Torino","2008GnTo", "HD");
+		Film TeUlSs = new BluRay("The Usual Suspects","1995TeUlSs", "HD");
+		
+		
+		//dodawanie VHS     // ('nazwa filmu','sygnatura filmu', 'informacja czy przewinieto film')
+		Film DyHy = new VHS("Dirty Harry","1971DyHy", "yes"); 
+		Film MmFe = new VHS("Magnum Force","1973MmFe", "no"); 
+		Film TeRe = new VHS("The Rookie","1990TeRe", "yes");
+		
 				
-		//VHS
-		Klient bob = new Klient("rl", "811346", "Dupa 45");
-		Film pupa = new VHS("pupa","2012pup", "yes");
-		bob.getFilms().add(pupa);
-		pupa.setOwner(bob);
-		hib.save(bob); 
+		//przypisanie klienta do filmu
+		TD.setOwner(jan_kow);
+		SrWs.setOwner(mar_now);
+		DyHy.setOwner(ste_bob);
+		MmFe.setOwner(ste_bob);
+		GnTo.setOwner(ann_kwi);
+		AePr.setOwner(paw_fle);
+		TeRe.setOwner(ste_bob);
+		Un.setOwner(ola_mal);
+		TeUlSs.setOwner(mir_wie);
 		
 		
+		//przypisywanie filmu do klienta
+		jan_kow.getFilms().add(TD);
+		mar_now.getFilms().add(SrWs);
+		ste_bob.getFilms().add(DyHy);
+		ste_bob.getFilms().add(MmFe);
+		ann_kwi.getFilms().add(GnTo);
+		paw_fle.getFilms().add(AePr);
+		ste_bob.getFilms().add(TeRe);
+		ola_mal.getFilms().add(Un);
+		mir_wie.getFilms().add(TeUlSs);
 		
+		
+		//zapisanie danych klientow
+		hib.save(jan_kow);
+		hib.save(mar_now); 
+		hib.save(ste_bob); 
+		hib.save(ann_kwi);
+		hib.save(paw_fle);
+		hib.save(ola_mal);
+		hib.save(mir_wie);
+	
 		
 		List<Klient>results = hib.getAll();
 		
